202ca8d964eadcfe98dc2e436c4d41cc858fa5f5
Rishabh <dingliwalr@gmail.com>
2021-05-21 17:32:38 +0530

    Add features for house price pred

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 import sklearn
 import datetime
 import warnings
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from flask import Flask, render_template, redirect, url_for, request
 
 warnings.filterwarnings('ignore')
@@ -129,7 +130,69 @@ def rock_paper_scissors():
 @app.route("/house-price-prediction/predict", methods=['GET', 'POST'])
 def house_price_prediction_predict():
     if request.method == 'POST':
-        prediction_text = ""
+        models = pickle.load(open('saved_models/house_price_prediction/models.pkl', 'rb'))
+        minmax_scaler = pickle.load(open('saved_models/house_price_prediction/minmax_scaler.pkl', 'rb'))
+        label_encoder_dict = pickle.load(open('saved_models/house_price_prediction/label_encoders.pkl', 'rb'))
+
+        GrLivArea = float(request.form['GrLivArea'])
+        stFlrSF = float(request.form['1stFlrSF'])
+        YearRemodAdd = int(request.form['YrSold']) - int(request.form['YearRemodAdd'])
+        MSZoning = request.form['MSZoning']
+        PavedDrive = request.form['PavedDrive']
+        BldgType = request.form['BldgType']
+        Fireplaces = int(request.form['Fireplaces'])
+        KitchenQual = request.form['KitchenQual']
+        GarageType = request.form['GarageType']
+        GarageFinish = request.form['GarageFinish']
+        GarageCars = int(request.form['GarageCars'])
+        CentralAir = request.form['CentralAir']
+        BsmtFullBath = int(request.form['BsmtFullBath'])
+        BsmtExposure = request.form['BsmtExposure']
+        BsmtFinType1 = request.form['BsmtFinType1']
+        BsmtQual = request.form['BsmtQual']
+        HeatingQC = request.form['HeatingQC']
+        ExterQual = request.form['ExterQual']
+        LotShape = request.form['LotShape']
+        OverallQual = int(request.form['OverallQual'])
+        Algorithm = request.form['Algorithm']
+
+        if MSZoning == 'C (all)':
+            MSZoning = 'Rare_Var'
+        if LotShape == 'IR3':
+            LotShape = 'Rare_Var'
+        if ExterQual == 'Fa':
+            ExterQual = 'Rare_Var'
+        if HeatingQC == 'Po':
+            HeatingQC = 'Rare_Var'
+        
+        MSZoning = label_encoder_dict['MSZoning'].transform([MSZoning])[0]
+        LotShape = label_encoder_dict['LotShape'].transform([LotShape])[0]
+        BldgType = label_encoder_dict['BldgType'].transform([BldgType])[0]
+        ExterQual = label_encoder_dict['ExterQual'].transform([ExterQual])[0]
+        BsmtQual = label_encoder_dict['BsmtQual'].transform([BsmtQual])[0]
+        BsmtExposure = label_encoder_dict['BsmtExposure'].transform([BsmtExposure])[0]
+        BsmtFinType1 = label_encoder_dict['BsmtFinType1'].transform([BsmtFinType1])[0]
+        HeatingQC = label_encoder_dict['HeatingQC'].transform([HeatingQC])[0]
+        CentralAir = label_encoder_dict['CentralAir'].transform([CentralAir])[0]
+        KitchenQual = label_encoder_dict['KitchenQual'].transform([KitchenQual])[0]
+        GarageFinish = label_encoder_dict['GarageType'].transform([GarageType])[0]
+        GarageFinish = label_encoder_dict['GarageFinish'].transform([GarageFinish])[0]
+        PavedDrive = label_encoder_dict['PavedDrive'].transform([PavedDrive])[0]
+        
+        X_test = [MSZoning, LotShape, BldgType, OverallQual, YearRemodAdd, ExterQual, BsmtQual, BsmtExposure, BsmtFinType1, HeatingQC, 
+                  CentralAir , stFlrSF, GrLivArea, BsmtFullBath, KitchenQual, Fireplaces, GarageType, GarageFinish, GarageCars, PavedDrive]
+                    
+
+        if Algorithm == 'linear_regressor':
+            prediction = models[0].predict(X_test)[0]
+        elif Algorithm == 'support_vector_regressor':
+            prediction = models[1].predict(X_test)[0]
+        elif Algorithm == 'random_forest_regressor':
+            prediction = models[2].predict(X_test)[0]
+        else:
+            prediction = models[3].predict(X_test)[0]
+        
+        prediction_text = "The estimated price of the house is ${}".format(prediction*100000)
         return render_template("predict/house_price_prediction.html", prediction_text=prediction_text, scroll='OutputPredictionText')
     return redirect(url_for('house_price_prediction'))
 
