a18fb51ed59f9d88f3f498f7c1ef1caabb7dbd1e
Rishabh <dingliwalr@gmail.com>
2021-06-05 16:35:51 +0530

    Use s3 bucket links for static files

--- url_dict.py
+++ url_dict.py
@@ -1,3 +1,5 @@
+root_url = "https://the-ml-dl-app-bucket.s3.amazonaws.com/static/statistics_images/"
+
 url_dict = {
     # House Price Prediction
     '1' : {
@@ -5,26 +7,26 @@ url_dict = {
         "dataset_link" : "https://www.kaggle.com/c/house-prices-advanced-regression-techniques",
         "stats_images_list" : {
             "Bar Plots" : [
-                "/static/statistics_images/1_bar_1.png", "/static/statistics_images/1_bar_2.png",
-                "/static/statistics_images/1_bar_3.png", "/static/statistics_images/1_bar_4.png",
-                "/static/statistics_images/1_bar_5.png", "/static/statistics_images/1_bar_6.png",
-                "/static/statistics_images/1_bar_7.png", "/static/statistics_images/1_bar_8.png",
-                "/static/statistics_images/1_bar_9.png", "/static/statistics_images/1_bar_10.png"
+                f"{root_url}1_bar_1.png", f"{root_url}1_bar_2.png",
+                f"{root_url}1_bar_3.png", f"{root_url}1_bar_4.png",
+                f"{root_url}1_bar_5.png", f"{root_url}1_bar_6.png",
+                f"{root_url}1_bar_7.png", f"{root_url}1_bar_8.png",
+                f"{root_url}1_bar_9.png", f"{root_url}1_bar_10.png"
             ],
             "Box Plots" : [
-                "/static/statistics_images/1_box_1.png", "/static/statistics_images/1_box_2.png",
-                "/static/statistics_images/1_box_3.png", "/static/statistics_images/1_box_4.png",
-                "/static/statistics_images/1_box_5.png", "/static/statistics_images/1_box_6.png",
-                "/static/statistics_images/1_box_7.png", "/static/statistics_images/1_box_8.png",
-                "/static/statistics_images/1_box_9.png", "/static/statistics_images/1_box_10.png"
+                f"{root_url}1_box_1.png", f"{root_url}1_box_2.png",
+                f"{root_url}1_box_3.png", f"{root_url}1_box_4.png",
+                f"{root_url}1_box_5.png", f"{root_url}1_box_6.png",
+                f"{root_url}1_box_7.png", f"{root_url}1_box_8.png",
+                f"{root_url}1_box_9.png", f"{root_url}1_box_10.png"
             ],
             "Scatter Plots" : [
-                "/static/statistics_images/1_scatter_1.png", "/static/statistics_images/1_scatter_2.png",
-                "/static/statistics_images/1_scatter_3.png"
+                f"{root_url}1_scatter_1.png", f"{root_url}1_scatter_2.png",
+                f"{root_url}1_scatter_3.png"
             ],
             "Distibution Plots" : [
-                "/static/statistics_images/1_dist_1.png", "/static/statistics_images/1_dist_2.png",
-                "/static/statistics_images/1_dist_3.png"
+                f"{root_url}1_dist_1.png", f"{root_url}1_dist_2.png",
+                f"{root_url}1_dist_3.png"
             ]
         }
     },
@@ -35,24 +37,24 @@ url_dict = {
         "test_images_link" : "https://ia801408.us.archive.org/view_archive.php?archive=/17/items/mnist_digit_recognizer_dataset/test.zip",
         "stats_images_list" : {
             "Graphs" : [
-                "/static/statistics_images/2_graph_1.png", "/static/statistics_images/2_graph_2.png"
+                f"{root_url}2_graph_1.png", f"{root_url}2_graph_2.png"
             ],
             "Scalars" : [
-                "/static/statistics_images/2_scalar_1.png", "/static/statistics_images/2_scalar_2.png"
+                f"{root_url}2_scalar_1.png", f"{root_url}2_scalar_2.png"
             ],
             "Distributions" : [
-                "/static/statistics_images/2_dist_1.png", "/static/statistics_images/2_dist_2.png",
-                "/static/statistics_images/2_dist_3.png", "/static/statistics_images/2_dist_4.png",
-                "/static/statistics_images/2_dist_5.png", "/static/statistics_images/2_dist_6.png",
-                "/static/statistics_images/2_dist_7.png", "/static/statistics_images/2_dist_8.png",
-                "/static/statistics_images/2_dist_9.png", "/static/statistics_images/2_dist_10.png"
+                f"{root_url}2_dist_1.png", f"{root_url}2_dist_2.png",
+                f"{root_url}2_dist_3.png", f"{root_url}2_dist_4.png",
+                f"{root_url}2_dist_5.png", f"{root_url}2_dist_6.png",
+                f"{root_url}2_dist_7.png", f"{root_url}2_dist_8.png",
+                f"{root_url}2_dist_9.png", f"{root_url}2_dist_10.png"
             ],
             "Histograms" : [
-                "/static/statistics_images/2_hist_1.png", "/static/statistics_images/2_hist_2.png",
-                "/static/statistics_images/2_hist_3.png", "/static/statistics_images/2_hist_4.png",
-                "/static/statistics_images/2_hist_5.png", "/static/statistics_images/2_hist_6.png",
-                "/static/statistics_images/2_hist_7.png", "/static/statistics_images/2_hist_8.png",
-                "/static/statistics_images/2_hist_9.png", "/static/statistics_images/2_hist_10.png"
+                f"{root_url}2_hist_1.png", f"{root_url}2_hist_2.png",
+                f"{root_url}2_hist_3.png", f"{root_url}2_hist_4.png",
+                f"{root_url}2_hist_5.png", f"{root_url}2_hist_6.png",
+                f"{root_url}2_hist_7.png", f"{root_url}2_hist_8.png",
+                f"{root_url}2_hist_9.png", f"{root_url}2_hist_10.png"
             ]
         }
     },
@@ -62,27 +64,27 @@ url_dict = {
         "test_images_link" : "https://ia803402.us.archive.org/view_archive.php?archive=/11/items/rock-paper-scissors-dataset/rps-test-set.zip",
         "stats_images_list" : {
             "Graphs" : [
-                "/static/statistics_images/3_graph_1.png", "/static/statistics_images/3_graph_2.png"
+                f"{root_url}3_graph_1.png", f"{root_url}3_graph_2.png"
             ],
             "Scalars" : [
-                "/static/statistics_images/3_scalar_1.png", "/static/statistics_images/3_scalar_2.png",
-                "/static/statistics_images/3_scalar_3.png", "/static/statistics_images/3_scalar_4.png"
+                f"{root_url}3_scalar_1.png", f"{root_url}3_scalar_2.png",
+                f"{root_url}3_scalar_3.png", f"{root_url}3_scalar_4.png"
             ],
             "Distributions" : [
-                "/static/statistics_images/3_dist_1.png", "/static/statistics_images/3_dist_2.png",
-                "/static/statistics_images/3_dist_3.png", "/static/statistics_images/3_dist_4.png",
-                "/static/statistics_images/3_dist_5.png", "/static/statistics_images/3_dist_6.png",
-                "/static/statistics_images/3_dist_7.png", "/static/statistics_images/3_dist_8.png",
-                "/static/statistics_images/3_dist_9.png", "/static/statistics_images/3_dist_10.png",
-                "/static/statistics_images/3_dist_11.png", "/static/statistics_images/3_dist_12.png"
+                f"{root_url}3_dist_1.png", f"{root_url}3_dist_2.png",
+                f"{root_url}3_dist_3.png", f"{root_url}3_dist_4.png",
+                f"{root_url}3_dist_5.png", f"{root_url}3_dist_6.png",
+                f"{root_url}3_dist_7.png", f"{root_url}3_dist_8.png",
+                f"{root_url}3_dist_9.png", f"{root_url}3_dist_10.png",
+                f"{root_url}3_dist_11.png", f"{root_url}3_dist_12.png"
             ],
             "Histograms" : [
-                "/static/statistics_images/3_hist_1.png", "/static/statistics_images/3_hist_2.png",
-                "/static/statistics_images/3_hist_3.png", "/static/statistics_images/3_hist_4.png",
-                "/static/statistics_images/3_hist_5.png", "/static/statistics_images/3_hist_6.png",
-                "/static/statistics_images/3_hist_7.png", "/static/statistics_images/3_hist_8.png",
-                "/static/statistics_images/3_hist_9.png", "/static/statistics_images/3_hist_10.png",
-                "/static/statistics_images/3_hist_11.png", "/static/statistics_images/3_hist_12.png"
+                f"{root_url}3_hist_1.png", f"{root_url}3_hist_2.png",
+                f"{root_url}3_hist_3.png", f"{root_url}3_hist_4.png",
+                f"{root_url}3_hist_5.png", f"{root_url}3_hist_6.png",
+                f"{root_url}3_hist_7.png", f"{root_url}3_hist_8.png",
+                f"{root_url}3_hist_9.png", f"{root_url}3_hist_10.png",
+                f"{root_url}3_hist_11.png", f"{root_url}3_hist_12.png"
             ]
         }
     },
@@ -92,15 +94,15 @@ url_dict = {
         "dataset_link" : "https://www.kaggle.com/ritesaluja/bank-note-authentication-uci-data",
         "stats_images_list" : {
             "Box Plots" : [
-                "/static/statistics_images/4_box_1.png", "/static/statistics_images/4_box_2.png",
-                "/static/statistics_images/4_box_3.png", "/static/statistics_images/4_box_4.png"
+                f"{root_url}4_box_1.png", f"{root_url}4_box_2.png",
+                f"{root_url}4_box_3.png", f"{root_url}4_box_4.png"
             ],
             "Count Plots" : [
-                "/static/statistics_images/4_count_1.png"
+                f"{root_url}4_count_1.png"
             ],
             "Distibution Plots" : [
-                "/static/statistics_images/4_dist_1.png", "/static/statistics_images/4_dist_2.png",
-                "/static/statistics_images/4_dist_3.png", "/static/statistics_images/4_dist_4.png"
+                f"{root_url}4_dist_1.png", f"{root_url}4_dist_2.png",
+                f"{root_url}4_dist_3.png", f"{root_url}4_dist_4.png"
             ]
         }
     },
@@ -110,19 +112,19 @@ url_dict = {
         "dataset_link" : "https://www.kaggle.com/nehalbirla/vehicle-dataset-from-cardekho",
         "stats_images_list" : {
             "Bar Plots" : [
-                "/static/statistics_images/5_bar_1.png", "/static/statistics_images/5_bar_2.png",
-                "/static/statistics_images/5_bar_3.png", "/static/statistics_images/5_bar_4.png"
+                f"{root_url}5_bar_1.png", f"{root_url}5_bar_2.png",
+                f"{root_url}5_bar_3.png", f"{root_url}5_bar_4.png"
             ],
             "Box Plots" : [
-                "/static/statistics_images/5_box_1.png", "/static/statistics_images/5_box_2.png",
-                "/static/statistics_images/5_box_3.png", "/static/statistics_images/5_box_4.png"
+                f"{root_url}5_box_1.png", f"{root_url}5_box_2.png",
+                f"{root_url}5_box_3.png", f"{root_url}5_box_4.png"
             ],
             "Scatter Plots" : [
-                "/static/statistics_images/5_scatter_1.png", "/static/statistics_images/5_scatter_2.png"
+                f"{root_url}5_scatter_1.png", f"{root_url}5_scatter_2.png"
             ],
             "Distibution Plots" : [
-                "/static/statistics_images/5_dist_1.png", "/static/statistics_images/5_dist_2.png",
-                "/static/statistics_images/5_dist_3.png", "/static/statistics_images/5_dist_4.png"
+                f"{root_url}5_dist_1.png", f"{root_url}5_dist_2.png",
+                f"{root_url}5_dist_3.png", f"{root_url}5_dist_4.png"
             ]
         }
     }   
